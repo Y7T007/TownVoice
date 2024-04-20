c87af6926f82dc3f1ac714c218a048e89916c993
y7t007 <yassir7t@gmail.com>
2024-04-20 12:19:36 +0100

    Linked login and register with ipfs database

--- internal/models/auth/client.go
+++ internal/models/auth/client.go
@@ -1,39 +1,82 @@
 package auth
 
+import (
+	"encoding/json"
+	"github.com/ipfs/go-ipfs-api"
+	"io/ioutil"
+	"log"
+	"strings"
+)
+
 type Client struct {
-	ID          int    `json:"id"`
-	Name        string `json:"name"`
-	Email       string `json:"email"`
-	Password    string `json:"password"`
-	City        string `json:"city"`
-	Address     string `json:"address"`
-	Gender      string `json:"gender"`
-	PhoneNumber string `json:"phone_number"`
-	// OtherDetails can be represented as a map[string]interface{} if the fields are dynamic
+	ID           int                    `json:"id"`
+	Name         string                 `json:"name"`
+	Email        string                 `json:"email"`
+	Password     string                 `json:"password"`
+	City         string                 `json:"city"`
+	Address      string                 `json:"address"`
+	Gender       string                 `json:"gender"`
+	PhoneNumber  string                 `json:"phone_number"`
 	OtherDetails map[string]interface{} `json:"other_details"`
+	CID          string                 `json:"cid"` // Add a CID field to store the IPFS CID
 }
 
 func SaveClient(user *Client) error {
+	// Connect to the ipfs daemon
+	sh := shell.NewShell("localhost:5001")
 
-	// Save the client to the database (or in this case, IPFS)
-	// For now, we will just print the client to the console
-	println("Saving client to the database:")
-	println("ID:", user.ID)
-	println("Name:", user.Name)
-	println("Email:", user.Email)
-	println("City:", user.City)
-	println("Address:", user.Address)
+	// Convert the client to JSON
+	clientJson, err := json.Marshal(user)
+	if err != nil {
+		return err
+	}
+
+	// Add the client to IPFS
+	cid, err := sh.Add(strings.NewReader(string(clientJson)))
+	if err != nil {
+		return err
+	}
+
+	// Store the CID in the user object
+	user.CID = cid
+
+	log.Printf("Added client %s to IPFS with CID %s", user.Email, cid)
+
 	return nil
 }
-
 func AuthenticateClient(user *Client) bool {
-	// Authenticate the client
-	// For now, we will just print the client to the console
-	println("Authenticating client:")
-	println("ID:", user.ID)
-	println("Name:", user.Name)
-	println("Email:", user.Email)
-	println("City:", user.City)
-	println("Address:", user.Address)
-	return true
+	// Connect to the ipfs daemon
+	sh := shell.NewShell("localhost:5001")
+
+	// Get the client from IPFS
+	readCloser, err := sh.Cat(user.CID)
+	if err != nil {
+		log.Printf("Error retrieving client from IPFS: %v", err)
+		return false
+	}
+	defer readCloser.Close()
+
+	// Read the data from the io.ReadCloser into a []byte
+	clientJson, err := ioutil.ReadAll(readCloser)
+	if err != nil {
+		log.Printf("Error reading client data: %v", err)
+		return false
+	}
+
+	// Convert the JSON to a client
+	var client Client
+	err = json.Unmarshal(clientJson, &client)
+	if err != nil {
+		log.Printf("Error unmarshalling client JSON: %v", err)
+		return false
+	}
+
+	log.Printf("Retrieved client %s from IPFS with CID %s", client.Email, user.CID)
+
+	// Compare the retrieved client with the input client
+	if client.Email == user.Email && client.Password == user.Password {
+		return true
+	}
+
+	return false
 }

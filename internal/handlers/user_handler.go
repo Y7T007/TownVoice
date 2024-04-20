821284a0cafa5f2b8fa90e406e8476bd73a584b6
Y7T007 <yassir7t@gmail.com>
2024-04-20 03:00:17 +0100

    The basic conceptions files are all added to the project source

--- internal/handlers/user_handler.go
+++ internal/handlers/user_handler.go
@@ -4,13 +4,25 @@ import (
 	"TownVoice/internal/auth"
 	"fmt"
 	"net/http"
+	"os"
+	"path/filepath"
 )
 
 func LoginHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case "GET":
+		// Get the current working directory
+		cwd, err := os.Getwd()
+		if err != nil {
+			http.Error(w, "Server error", http.StatusInternalServerError)
+			return
+		}
+
+		// Construct the absolute path to the login.html file
+		loginFilePath := filepath.Join(cwd, "web", "login.html")
+
 		// Render the login form
-		http.ServeFile(w, r, "./web/login.html")
+		http.ServeFile(w, r, loginFilePath)
 	case "POST":
 		// Handle the form submission
 		username := r.FormValue("username")

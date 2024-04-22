c535921c6a23dfde5e8b86e52148ceca89c009d9
y7t007 <yassir7t@gmail.com>
2024-04-22 02:17:40 +0100

    saving comment to the IPFS

--- internal/facade/commentsFacade.go
+++ internal/facade/commentsFacade.go
@@ -1,6 +1,13 @@
 package facade
 
-import "log"
+import (
+	"TownVoice/internal/models"
+	"TownVoice/internal/repositories/databasesRepo"
+	"encoding/json"
+	"log"
+	"strings"
+	"time"
+)
 
 func GetCommentsByEntity(entityID string) {
 
@@ -12,9 +19,35 @@ func GetCommentsByUser(userID string) {
 }
 
 func AddComment(entityID string, comment string, uid string) {
-
-	//	print that its received
-
+	// Print that it's received
 	log.Printf("From the facade : User with UID %s and  added a comment on entity %s: %s\n", uid, entityID, comment)
 
+	// Create a new Comment object
+	newComment := models.Comment{
+		UserID:    uid,
+		EntityID:  entityID,
+		Content:   comment,
+		Timestamp: time.Now(),
+	}
+
+	// Convert the Comment object to JSON
+	commentJSON, err := json.Marshal(newComment)
+	if err != nil {
+		log.Fatalf("Failed to convert comment to JSON: %v", err)
+	}
+
+	// Print the JSON of the new comment
+	log.Printf("Comment JSON: %s\n", commentJSON)
+
+	// Get a new IPFSRepo
+	ipfsRepo := databasesRepo.NewIPFSRepo()
+
+	// Add the comment JSON to IPFS
+	cid, err := ipfsRepo.Shell.Add(strings.NewReader(string(commentJSON)))
+	if err != nil {
+		log.Fatalf("Failed to add comment to IPFS: %v", err)
+	}
+
+	// Print the CID of the new IPFS object
+	log.Printf("Added comment to IPFS with CID %s\n", cid)
 }

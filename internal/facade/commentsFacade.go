869e8a8908149531c621c8e64655f450a84f6890
y7t007 <yassir7t@gmail.com>
2024-04-22 03:02:23 +0100

    Updated CIds working in firestore

--- internal/facade/commentsFacade.go
+++ internal/facade/commentsFacade.go
@@ -3,6 +3,7 @@ package facade
 import (
 	"TownVoice/internal/models"
 	"TownVoice/internal/repositories/databasesRepo"
+	"cloud.google.com/go/firestore"
 	"context"
 	"encoding/json"
 	"log"
@@ -20,9 +21,8 @@ func GetCommentsByUser(userID string) {
 }
 
 func AddComment(entityID string, comment string, uid string) {
-
 	// Print that it's received
-	log.Printf("From the facade : User with UID %s and  added a comment on entity %s: %s\n", uid, entityID, comment)
+	log.Printf("From the facade: User with UID %s added a comment on entity %s: %s\n", uid, entityID, comment)
 
 	// Create a new Comment object
 	newComment := models.Comment{
@@ -53,16 +53,41 @@ func AddComment(entityID string, comment string, uid string) {
 	// Print the CID of the new IPFS object
 	log.Printf("Added comment to IPFS with CID %s\n", cid)
 
-	//	add the cid to firestore
+	// Add the cid to Firestore
 	firestoreRepo := databasesRepo.NewFirestoreRepo()
-
 	ctx := context.Background()
 
-	_, err = firestoreRepo.Client.Collection("Comments").Doc(entityID).Set(ctx, map[string]string{"cid": cid})
+	// Fetch existing document
+	docRef := firestoreRepo.Client.Collection("Comments").Doc(entityID)
+	docSnap, err := docRef.Get(ctx)
 	if err != nil {
-		log.Fatalf("Failed to add CID to Firestore: %v", err)
+		log.Fatalf("Failed to fetch document: %v", err)
 	}
+
+	// Extract existing cids from the document
+	var existingCids []string
+	if docSnap.Exists() {
+		data := docSnap.Data()
+		if data != nil {
+			if cidList, ok := data["cids"].([]interface{}); ok {
+				for _, c := range cidList {
+					if cid, ok := c.(string); ok {
+						existingCids = append(existingCids, cid)
+					}
+				}
+			}
+		}
+	}
+
+	// Append the new cid to existing cids
+	existingCids = append(existingCids, cid)
+
+	// Update the Firestore document with the new cids
+	_, err = docRef.Set(ctx, map[string]interface{}{"cids": existingCids}, firestore.MergeAll)
+	if err != nil {
+		log.Fatalf("Failed to update Firestore document: %v", err)
+	}
+
 	// Print the CID of the new IPFS object
 	log.Printf("Added CID to Firestore with CID %s\n", cid)
-
 }

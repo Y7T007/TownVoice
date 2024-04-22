8df9ea86a1f76f5b8b78f421c1ce3d3742f7dab2
y7t007 <yassir7t@gmail.com>
2024-04-23 00:59:00 +0100

    added modal

--- internal/models/rating.go
+++ internal/models/rating.go
@@ -1,1 +1,7 @@
 package models
+
+type Rating struct {
+	EntityID string             `json:"entity_id"`
+	UserID   string             `json:"user_id"`
+	Scores   map[string]float64 `json:"scores"`
+}

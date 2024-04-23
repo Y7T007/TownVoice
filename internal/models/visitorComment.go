e85790f35a7e2394163a7784f06d917e65046cec
y7t007 <yassir7t@gmail.com>
2024-04-23 01:31:28 +0100

    Implementing the model

--- internal/models/visitorComment.go
+++ internal/models/visitorComment.go
@@ -1,1 +1,24 @@
 package models
+
+import (
+	"log"
+	"strings"
+)
+
+type Visitor interface {
+	VisitComment(*Comment)
+}
+
+type BadWordDetector struct {
+	badWords []string
+}
+
+func (v *BadWordDetector) VisitComment(c *Comment) {
+	for _, word := range v.badWords {
+		if strings.Contains(strings.ToLower(c.Content), word) {
+			log.Printf("Comment contains sensitive content. Not adding to blockchain.")
+			c.Content = "" // Clear the content or handle it as you see fit
+			return
+		}
+	}
+}
